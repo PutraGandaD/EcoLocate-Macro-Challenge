281e8bcf91963649cf9bf6c28b81d8d0e1294b2b
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-10-12 01:57:18 +0700

    Added Landing Page Activity + finished FE
    - Added custom Font : Inter
    - Added custom Button drawable
    - Used ViewBinding for MainActivity and it's component
    - Successfully tested intent
    - Added some icons
    - more...

--- app/src/main/java/org/ecogank/ecolocate/MainActivity.kt
+++ app/src/main/java/org/ecogank/ecolocate/MainActivity.kt
@@ -1,12 +1,40 @@
 package org.ecogank.ecolocate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import org.ecogank.ecolocate.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        supportActionBar?.hide()
+
+        setContentView(binding.root)
+
+        val btnLogin: Button = binding.btnLogin
+        btnLogin.setOnClickListener(this)
+
+        val btnRegister: Button = binding.btnRegister
+        btnRegister.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.btn_login -> {
+                val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.btn_register -> {
+                val intent = Intent(this@MainActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
