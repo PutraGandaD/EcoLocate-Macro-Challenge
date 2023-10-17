db57f2ccbd543e1f3cae7bdba4b604b0d87d0533
Novia Indah Ramadhani <noviairamadhani@gmail.com>
2023-10-18 04:44:31 +0800

    Edit Profil

--- app/src/main/java/org/ecogank/ecolocate/HomeActivity.kt
+++ app/src/main/java/org/ecogank/ecolocate/HomeActivity.kt
@@ -30,6 +30,7 @@ class HomeActivity : AppCompatActivity() {
                 R.id.temukan -> replaceFragment(TemukanFragment())
                 R.id.pojokedu -> replaceFragment(PojokEdukasiFragment())
                 R.id.profil -> replaceFragment(ProfilFragment())
+//                R.id.profil -> replaceFragment(ProfilEditFragment())
                 else -> {
 
                 }
